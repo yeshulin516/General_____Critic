a4ece645227febfc1b0f2addb78a18e842af3f17
“Shulin <ernestwoods0516@gmail.com>
2017-11-19 13:36:19 -0500

    display all, 3.5 hours

--- GeneralCritic/index.js
+++ GeneralCritic/index.js
@@ -17,6 +17,10 @@ MongoClient.connect(url, function(err, db) {
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
 
 // routes will go here
 
@@ -58,27 +62,30 @@ app.get('/write-req', function(req, res) {
 });
 
 app.get('/read-req', function(req, res) {
-  //console.log(URL.parse(req.url, true).query);
-  MongoClient.connect(url, function(err, db) {
-  var search = URL.parse(req.url, true).query.Search;
-  search = search.substring(0, search.length - 2);
-  //console.log(search);
-  var reviews = db.collection('Reviews').find();
-  reviews.each(function(err, item) {
-        if (item != null){
-          var title = item.Title;
-          title = title.substring(0, title.length - 4);
-          console.log(title);
-          console.log(search);
-          
-          if (title.valueOf() === search.valueOf()){
-            console.log(item);
+  MongoClient.connect(url, function (err, db) {
+    var reviews = db.collection('Reviews').find();
+    var search = URL.parse(req.url, true).query.Search;
+    search = search.substring(0, search.length - 2);
+    var sendObj = {};
+    var items = [];
+    var i = 1;
+    reviews.each(function(err, item) {
+          if (item != null){
+            var title = item.Title;
+            title = title.substring(0, title.length - 4);
+            if (title.valueOf() === search.valueOf()|| search.valueOf() === "".valueOf()){
+              items.push(item);
+              i = i + 1;
+            }
           }
-        }
-      });
-  db.close();
+          else{
+            sendObj["items"] = items;
+            res.render('respBody', sendObj);
+            db.close();
+          }
+        });
+    
   });
-  res.redirect('/');
 });
 
 
